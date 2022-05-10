e626594e4e60ba0c979a563edcd4d1475df21743
pauloschade <paulosc4@al.insper.edu.br>
2022-05-10 17:04:38 -0300

    new version

--- test/ERC721Capped.test.js
+++ test/ERC721Capped.test.js
@@ -1,10 +1,10 @@
-const truffleAssert = require("truffle-assertions");
-const { BN } = require('@openzeppelin/test-helpers');
+const truffleAssert = require("truffle-assertions");;
+const Web3 = require('web3');
 const ERC721Capped = artifacts.require("./ERC721Capped");
 
 contract('ERC721Capped', (accounts) => {
     const [ owner, spender, recipient, other ] = accounts;
-
+    let web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
     var capped;
     cap = 20;
 
@@ -30,7 +30,7 @@ contract('ERC721Capped', (accounts) => {
 
     describe("Open sales", () => {
         beforeEach(async () =>  {
-            newPrice = new BN(0.2);
+            newPrice = web3.utils.toWei("0.2");
             await truffleAssert.passes(capped.openSales(newPrice));
         });
         it("Open sales and allows minting for public", async () => {
@@ -41,7 +41,7 @@ contract('ERC721Capped', (accounts) => {
             assert.equal(totalSupply.toString(), '10');
         })
         it("Changes the price", async () => {
-            newestPrice = new BN(1);
+            newestPrice = web3.utils.toWei("1");
             await truffleAssert.passes(capped.setMintPrice(newestPrice));
 
             await truffleAssert.fails(capped.publicMint({from: spender, value: newPrice}));
@@ -63,13 +63,36 @@ contract('ERC721Capped', (accounts) => {
 
     describe("Cap", () => {
         it("Doesnt allow minting after cap is reached", async () => {
-            price = new BN(0.01);
-            capped.openSales(price)
+            price = web3.utils.toWei("0.01");
+            await capped.openSales(price)
             for(i = 0; i < cap; i++) {
                 await capped.ownerMint();
             }
             await truffleAssert.fails(capped.ownerMint());
             await truffleAssert.fails(capped.publicMint({from: other, value: price}));
+        })
+    })
+
+    describe("Balance", () => {
+        it("Checks contract balance then, transferns to owner", async () => {
+            price = web3.utils.toWei("0.1");
+            await capped.openSales(price);
+            iteractions = 10
+            for(i = 0; i < iteractions; i++) {
+                await truffleAssert.passes(capped.publicMint({from: spender, value: price}));
+            }
+            const contractBalance = await capped.getBalance();
+            assert.equal(contractBalance, price * iteractions);
+
+            const prevOwnerBalance = await web3.eth.getBalance(owner)
+            await capped.withdraw();
+            const currOwnerBalance = await web3.eth.getBalance(owner)
+
+            const prevOwnerBalanceEth = parseFloat(web3.utils.fromWei(prevOwnerBalance, "ether" ))
+            const currOwnerBalanceEth = parseFloat(web3.utils.fromWei(currOwnerBalance, "ether" ))
+            const contractBalanceEth =  parseFloat(web3.utils.fromWei(contractBalance, "ether" ))
+            assert(currOwnerBalanceEth > prevOwnerBalanceEth);
+            assert(currOwnerBalanceEth <= prevOwnerBalanceEth + contractBalanceEth);
         })
     })
 
