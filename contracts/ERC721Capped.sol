a99a94f1f4b94b733fccec0b615431bdfcf7c356
pauloschade <62723600+pauloschade@users.noreply.github.com>
2022-05-09 21:52:08 -0300

    Update ERC721Capped.sol

--- contracts/ERC721Capped.sol
+++ contracts/ERC721Capped.sol
@@ -5,37 +5,5 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ERC721Capped is ERC721 {
 
-    constructor(string memory name, string memory symbol, uint64 cap) ERC721(name, symbol) {
-        //TODO
-    }
-
-    function _createNFT()
-        internal
-    {
-        //TODO
-
-    }
-
-    function ownerMint() public {
-    }
-
-    function publicMint() public {
-        //TODO
-    }
-
-    function getTotalSupply() public {
-        //TODO
-    }
-
-    function setMintPrice() public {
-        //TODO
-    }
-
-    function openSales() public {
-        //TODO
-    }
-
-    function closeSales() public {
-        //TODO
-    }
+    constructor(string memory name, string memory symbol, uint64 cap);
 }
